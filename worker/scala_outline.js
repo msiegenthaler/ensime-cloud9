dafaed6b684a07405dc54b4db156632632e94c87
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-01-22 19:50:40 +0000

    Positions in outline.

--- worker/scala_outline.js
+++ worker/scala_outline.js
@@ -31,7 +31,9 @@ define(function(require, exports, module) {
       if (err) return callback(err);
       console.log(result); //TODO remove
 
-      var ol = result.view.map(convert);
+      var ol = result.view.map(function(e) {
+        return convert(doc, e);
+      });
       callback(false, {
         isGeneric: false,
         isUnordered: false,
@@ -40,38 +42,51 @@ define(function(require, exports, module) {
     });
   };
 
-  function convert(obj) {
-    return {
+  function convert(doc, obj) {
+    var pos = posFor(doc, obj);
+    var result = {
       icon: iconFor(obj.keyword),
       name: obj.name,
-      pos: posFor(obj),
-      displayPos: posFor(obj),
-      items: obj.members.map(convert)
+      pos: pos,
+      displayPos: pos
     };
+    if (obj.members && obj.members.length > 0)
+      result.items = obj.members.filter(function(e) {
+        return e.keyword;
+      }).map(function(e) {
+        return convert(doc, e);
+      });
+    return result;
   }
 
-  function posFor(obj) {
-    //TODO calculate from offset
-    return {
+  function posFor(doc, obj) {
+    if (!obj.position) return {
       sl: 0,
       sc: 0,
       el: 0,
       ec: 0
+    };
+    var pos = doc.indexToPosition(obj.position.offset);
+    return {
+      sl: pos.row,
+      sc: pos.column,
+      el: pos.row,
+      ec: pos.column
     };
   }
 
   function iconFor(kw) {
     switch (kw) {
       case "object":
-        return "event";
-      case "class":
-        return "method";
-      case "trait":
-        return "method2";
-      case "def":
         return "property";
-      case "val":
+      case "class":
         return "property2";
+      case "trait":
+        return "property2";
+      case "def":
+        return "event";
+      case "val":
+        return "method2";
       case "type":
         return "unknown2";
       default:
