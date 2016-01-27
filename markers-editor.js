b49cfb9e92522acdc632a149fe8469890da5bd65
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-01-27 22:13:13 +0000

    Allow to cycle through all errors.

--- markers-editor.js
+++ markers-editor.js
@@ -7,11 +7,40 @@ define(function(require, exports, module) {
         var commands = imports.commands;
         var path = require("path");
 
+        function sortMarkers(markers) {
+            function sn(severity) {
+                switch (severity) {
+                    case "error":
+                        return 100;
+                    case "warning":
+                        return 10;
+                    case "info":
+                        return 1;
+                    default:
+                        return 0;
+                }
+            }
+
+            function order(a, b) {
+                var asn = sn(a.type);
+                var bsn = sn(b.type);
+                if (asn > bsn) return -1;
+                if (asn < bsn) return 1;
+                if (a.file < b.file) return -1;
+                if (a.file > b.file) return 1;
+                if (a.pos.sl < b.pos.sl) return -1;
+                if (a.pos.sl > b.pos.sl) return 1;
+                if (a.message == b.message) return 0;
+                return -1;
+            }
+            return markers.sort(order);
+        }
 
         function MarkersEditor() {
             console.log("initializing Scala MarkersEditor.");
 
             var currentMarkers = [];
+            var currentPos = -1;
 
             var plugin = new Editor("Ensime", consumes, []);
             plugin.on("draw", function(e) {
@@ -42,35 +71,7 @@ define(function(require, exports, module) {
                         },
                         width: "30%"
                     }],
-
-                    sort: function(markers) {
-                        function sn(severity) {
-                            switch (severity) {
-                                case "error":
-                                    return 100;
-                                case "warning":
-                                    return 10;
-                                case "info":
-                                    return 1;
-                                default:
-                                    return 0;
-                            }
-                        }
-
-                        function order(a, b) {
-                            var asn = sn(a.type);
-                            var bsn = sn(b.type);
-                            if (asn > bsn) return -1;
-                            if (asn < bsn) return 1;
-                            if (a.file < b.file) return -1;
-                            if (a.file > b.file) return 1;
-                            if (a.pos.sl < b.pos.sl) return -1;
-                            if (a.pos.sl > b.pos.sl) return 1;
-                            if (a.message == b.message) return 0;
-                            return -1;
-                        }
-                        return markers.sort(order);
-                    }
+                    sort: sortMarkers
                 }, plugin);
 
                 function jumpToMarker(marker) {
@@ -103,11 +104,27 @@ define(function(require, exports, module) {
                     },
                     exec: function() {
                         if (!currentMarkers || currentMarkers.length == 0) return;
-                        jumpToMarker(currentMarkers[0]);
+                        currentPos++;
+                        if (currentPos >= currentMarkers.length) currentPos = 0;
+                        jumpToMarker(currentMarkers[currentPos]);
+                        table.select(currentMarkers[currentPos]);
                     }
                 }, plugin);
 
                 ensime.on("markers", function(markers) {
+                    markers = sortMarkers(markers);
+                    if (currentPos > -1 && currentPos < currentMarkers.length) {
+                        //try to find our last position in the new markers
+                        var c = currentMarkers[currentPos];
+                        currentPos = markers.findIndex(function(e) {
+                            return c.fileFull === e.fileFull &&
+                                c.message === e.message &&
+                                c.type === e.type &&
+                                c.pos.sl === e.pos.sl &&
+                                c.pos.sc === e.pos.sc;
+                        });
+                    }
+                    else currentPos = -1;
                     currentMarkers = markers;
                     table.setRoot(markers);
                 }, plugin);
