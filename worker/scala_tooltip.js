227c5c2ef6879f4d06f7dfb0cb81c5520cd3b6f7
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-01-29 23:13:32 +0000

    WIP: run into a problem with ENSIME, reported as #1267.

--- worker/scala_tooltip.js
+++ worker/scala_tooltip.js
@@ -36,19 +36,42 @@ define(function(require, exports, module) {
   };
 
 
-  handler.tooltip = function(doc, ast, cursorPos, options, callback) {
-    console.info("Requesting tooltip for " + handler.path + ":" + JSON.stringify(cursorPos))
-    callback({
-      hint: "This is a <b>tooltip</b>",
-      // signatures: [{
-      // name: "Test"
-      // }],
-      pos: {
-        sl: cursorPos.row,
-        sc: cursorPos.column,
-        el: cursorPos.row,
-        ec: cursorPos.column
+  handler.tooltip = function(doc, ast, pos, options, callback) {
+    console.info("Requesting tooltip for " + handler.path + ":" + JSON.stringify(pos));
+
+    executeEnsime({
+      typehint: "DocUriAtPointReq",
+      file: {
+        file: handler.workspaceDir + handler.path,
+        currentContents: true
+      },
+      point: {
+        from: util.posToOffset(doc, pos),
+        to: util.posToOffset(doc, pos)
       }
+    }, function(err, result) {
+      if (err) {
+        console.error("Could not get the tooltip: " + err);
+        return callback();
+      }
+      if (result.typehint === "StringResponse") {
+        console.warn(result.text);
+      }
+      else {
+        console.warn("no doc");
+      }
+      callback({
+        hint: "This is a <b>tooltip</b>",
+        // signatures: [{
+        // name: "Test"
+        // }],
+        pos: {
+          sl: cursorPos.row,
+          sc: cursorPos.column,
+          el: cursorPos.row,
+          ec: cursorPos.column
+        }
+      });
     });
   };
 });
