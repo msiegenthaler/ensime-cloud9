5391bbdc69689ba8f09a9708251c38acee662421
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-02-07 20:31:32 +0000

    Implement in the workers.

--- worker/scala_jumptodefinition.js
+++ worker/scala_jumptodefinition.js
@@ -35,6 +35,7 @@ define(function(require, exports, module) {
       typehint: "SymbolAtPointReq",
       file: {
         file: handler.workspaceDir + options.path,
+        contents: doc.getValue(),
         currentContents: true
       },
       point: util.posToOffset(doc, pos)
