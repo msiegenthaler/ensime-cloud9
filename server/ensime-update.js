4b0023526de09a5103addb2595c7f20539ed202b
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-03-02 19:11:33 +0000

    Use logfile for ensime update.

--- server/ensime-update.js
+++ server/ensime-update.js
@@ -1,14 +1,6 @@
 var Controller = require("ensime-controller-js");
-
-var stdout = {
-  out: process.stderr,
-  err: process.stderr
-};
-
-// keep the stdout clean since we need it for messages.
-console.log = console.info = console.debug = console.warn = function(message) {
-  if (message) process.stderr.write(message.toString());
-};
+var fs = require("fs");
+var path = require("path");
 
 if (process.argv.length < 4) {
   return console.error("Specify the .ensime file and the location of sbt as command line argument.");
@@ -16,13 +8,28 @@ if (process.argv.length < 4) {
 var dotEnsime = process.argv[2];
 var sbt = process.argv[3];
 
-var ec = new Controller(dotEnsime, "/tmp/ensime", {sbt: sbt});
+var workspace = path.dirname(dotEnsime);
+var logfile = path.relative(workspace, ".ensime_cache/ensime.log");
+var log = fs.createWriteStream(logfile);
+var output = {
+  out: log,
+  err: log
+};
+
+// keep the stdout clean since we need it for messages.
+console.log = console.info = console.debug = console.warn = function(message) {
+  if (message) output.out.write(message.toString() + "\n");
+};
+
+var ec = new Controller(dotEnsime, "/tmp/ensime", {
+  sbt: sbt
+});
 
 ec.handleGeneral = function(msg) {
   process.stdout.write(JSON.stringify(msg));
 };
 
-ec.update(stdout, function(err, res) {
+ec.update(output, function(err, res) {
   if (err) return console.error(err);
   ec.handleGeneral({
     type: "updated",
